da90080a9cfd953c0806c1e9ea91d0fd892985ea
weerachit utnok <weerachit.utnok01@gmail.com>
2024-08-05 16:15:53 +0700

    fix: delete

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import preact from '@preact/preset-vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  server: {
+    proxy: {
+      '/v1': {
+        target: 'https://exp.host/--/api/v2/push/send',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
