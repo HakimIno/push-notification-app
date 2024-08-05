8202c985bc9161b7e7b96ad115654bf743df8c45
weerachit utnok <weerachit.utnok01@gmail.com>
2024-08-05 15:57:35 +0700

    fix: cros

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,4 @@ import preact from '@preact/preset-vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://exp.host/--/api/v2/push/send', // เปลี่ยน URL เป็น API ที่คุณต้องการ proxy
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
 })
