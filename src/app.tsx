8cc5f7169c6d4fce700a2c93fa1200c3dde56064
weerachit utnok <weerachit.utnok01@gmail.com>
2024-08-18 10:55:04 +0700

    fix: refactor code

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes, HashRouter as Router } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import { Theme } from '@radix-ui/themes';
